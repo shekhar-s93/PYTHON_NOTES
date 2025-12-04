2edbbdcbd28fca2a3a8c33428e89113ea0d7df25
shekhar-s93 <shekharsuman3266@gmail.com>
2025-12-04 21:27:40 +0530

    Dec 4, 2024 Updated

---
+++ 2_String and Conditional Statement/1_String/1_typesOfString.py
@@ -0,0 +1,21 @@
+# In python we have 3 types of written types of string.
+
+# 1_ type One
+
+str_1 = 'Shekhar'
+print("Type one: ", str_1)
+
+# 2_ type Two
+
+str_2 = "Hello Shekhar"
+print("Type two: ", str_2)
+
+# 3_ type Three
+
+str_3 = """Hello Shekhar Suman"""
+print("Type three: ", str_3)
+
+# If we want to give new line in string sentences then we have to use (\n), and if we want tabs in string sentences then we have to use (\t).
+
+str = "Hello mr. \nShekhar Suman \t How are you today?"
+print(str)
